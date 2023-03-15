8857277ea6d531a7a1d10155b1bdc044e75f5848
l.werner <l.werner@groupeonepoint.com>
2023-03-15 12:19:10 +0100

    [Feature] Implémentation localStorage service

--- src/helpers/NativeStorageHelper.ts
+++ src/helpers/NativeStorageHelper.ts
@@ -1,3 +1,17 @@
-export class NativeStorageHelper {
+import { IStorageHelper } from "./IStorageHelper";
+
+export class NativeStorageHelper<T> implements IStorageHelper<T> {
+
+    async get(key: string): Promise<T> {
+        throw new Error("Method not implemented.");
+    }
+
+    async set(key: string, value: T): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    async remove(key: string, value: T): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
     
 }
--- src/services/FavoriteLocationsService.ts
+++ src/services/FavoriteLocationsService.ts
@@ -1,3 +1,47 @@
+import { Constants } from "../constants/Constants";
+import { IStorageHelper } from "../helpers/IStorageHelper";
+import { LocalStorageHelper } from "../helpers/LocalStorageHelper";
+
 export class FavoriteLocationsService {
-    
+    private _helper: IStorageHelper<string>;
+
+    constructor() {
+        this._helper = new LocalStorageHelper();
+        // throw new Error("Method not implemented.");
+    }
+
+    async addFavoriteLocation(location: string): Promise<void> {
+
+        const favorites = await this.getFavoriteLocations();
+        
+        this._helper.set(
+            Constants.Storage.Keys.FavoriteLocations,
+            JSON.stringify(favorites 
+                ? [
+                    ...favorites,
+                    location
+                ] : [location]
+            )
+        )
+    }
+
+    async removeFavoriteLocation(location: string): Promise<void> {
+        const favorites = await this.getFavoriteLocations();
+
+        if (favorites) {
+            this._helper.set(
+                Constants.Storage.Keys.FavoriteLocations,
+                JSON.stringify(favorites.filter(el => el !== location))
+            )
+        }
+
+    }
+
+    async getFavoriteLocations(): Promise<string[] | null> {
+        const storedFavorites = await this._helper.get(Constants.Storage.Keys.FavoriteLocations);
+
+        if (!storedFavorites) return null;
+
+        return await JSON.parse(storedFavorites) as string[];
+    }
 }
