24cdb9f2c59603499a9c56458c425b6a1ef84484
Alexandre Bencheikh <alexandre.bencheikh@energy-go.fr>
2023-03-15 14:45:51 +0100

    [FEAT] Affichage de la temperature

--- src/pages/Groupe-APE/Exemple.tsx
+++ src/pages/Groupe-APE/Exemple.tsx
@@ -1,7 +1,41 @@
-export const GroupeApe = () => {
-    return (
-        <>
-        <h1>Groupe Ape</h1>
-        </>
-    )
+import React, { useEffect, useState } from 'react';
+
+interface ConversionData {
+    forecast : [{
+        datetime : string,
+        tsoil2: number
+    }]
 }
+const API_KEY = '45eb54d1f30b67a31d6aa41ab941d3e9d917b7f0f98e23bb125c9073deb69774';
+export const GroupeApe = () => {
+    const [conversionData, setConversionData] = useState<ConversionData | null>(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(
+                'https://api.meteo-concept.com/api/forecast/nextHours?token=45eb54d1f30b67a31d6aa41ab941d3e9d917b7f0f98e23bb125c9073deb69774&latlng=45.764043,4.835659', {
+                method: 'GET',
+            });
+            const data = await response.json();
+            setConversionData(data as ConversionData);
+        }
+
+        fetchData();
+    }, []);
+
+    if (!conversionData) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div>
+            <ul>
+            {conversionData.forecast.map((item) => (
+                <div><p>{item.datetime}</p>
+                <p>{item.tsoil2}</p>
+                </div>
+            ))}
+
+            </ul>
+        </div>
+    );
+};
