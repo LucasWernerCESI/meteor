bd6c0d6e940c24fba15b65e35181bc1ffaa1c108
t.loillieux <thomas.loillieux@gmail.com>
2023-03-15 13:43:26 +0100

    [feature] install NativeStorage

--- src/pages/Groupe-PFOU/Exemple.tsx
+++ src/pages/Groupe-PFOU/Exemple.tsx
@@ -1,7 +1,22 @@
+import { FavoriteLocationsService } from "../../services/FavoriteLocationsService"
+
 export const GroupePfou = () => {
-  return (
-    <>
-      <h1>Groupe Pfou</h1>
-    </>
-  );
-};
+    const service = new FavoriteLocationsService();
+
+    service.addFavoriteLocation("New York").then(
+        () => {service.getFavoriteLocations()
+            .then(response => {console.log(response)})}
+    )
+
+    console.log(service.getFavoriteLocations());
+
+    // service.removeFavoriteLocation("New York");
+
+    // console.log(service.getFavoriteLocations());
+
+    return (
+        <>
+        <h1>Groupe Pfou</h1>
+        </>
+    )
+}
