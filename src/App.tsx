f4be82e2a6ab0d184f8b50dede5193a09bcf7290
thibaultvoisin <thibault.voisin@koesio.com>
2023-03-15 10:11:51 +0100

    modif emote team

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import {
   IonApp,
   IonIcon,
@@ -7,33 +7,33 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, sparkles, square, triangle } from 'ionicons/icons';
+  setupIonicReact,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { egg, flower, sparkles, woman } from "ionicons/icons";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-import { GroupeApe } from './pages/Groupe-APE/Exemple';
-import { GroupeEuh } from './pages/Groupe-EUH/Exemple';
-import { GroupeFeur } from './pages/Groupe-FEUR/Exemple';
-import { GroupePfou } from './pages/Groupe-PFOU/Exemple';
+import "./theme/variables.css";
+import { GroupeApe } from "./pages/Groupe-APE/Exemple";
+import { GroupeEuh } from "./pages/Groupe-EUH/Exemple";
+import { GroupeFeur } from "./pages/Groupe-FEUR/Exemple";
+import { GroupePfou } from "./pages/Groupe-PFOU/Exemple";
 
 setupIonicReact();
 
@@ -52,23 +52,20 @@ const App: React.FC = () => (
             <GroupeFeur />
           </Route>
           <Route path="/pfou">
-            <GroupePfou/>
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
+            <GroupePfou />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="EUH" href="/euh">
-            <IonIcon aria-hidden="true" icon={triangle} />
+            <IonIcon aria-hidden="true" icon={woman} />
             <IonLabel>EUH</IonLabel>
           </IonTabButton>
           <IonTabButton tab="APE" href="/ape">
-            <IonIcon aria-hidden="true" icon={ellipse} />
+            <IonIcon aria-hidden="true" icon={egg} />
             <IonLabel>APE</IonLabel>
           </IonTabButton>
           <IonTabButton tab="FEUR" href="/feur">
-            <IonIcon aria-hidden="true" icon={square} />
+            <IonIcon aria-hidden="true" icon={flower} />
             <IonLabel>FEUR</IonLabel>
           </IonTabButton>
           <IonTabButton tab="PFOU" href="/pfou">
