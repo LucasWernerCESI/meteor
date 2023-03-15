2153c6e423af14fcbcde2e3c4829bda8c439bf88
thibaultvoisin <thibault.voisin@koesio.com>
2023-03-15 10:25:49 +0100

    feat header

--- src/pages/Groupe-EUH/Exemple.tsx
+++ src/pages/Groupe-EUH/Exemple.tsx
@@ -1,7 +1,10 @@
+import { AppHeader } from "../../components/Header/Header";
+
 export const GroupeEuh = () => {
-    return (
-        <>
-        <h1>Groupe Euh</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Groupe Euh</h1>
+      <AppHeader />
+    </>
+  );
+};
