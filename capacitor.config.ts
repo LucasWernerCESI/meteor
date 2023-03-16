fb9975eadbfd38db2abfa1776da0debca3221f80
t.loillieux <thomas.loillieux@gmail.com>
2023-03-16 09:59:25 +0100

    [feature] ios config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'meteor12345',
   appName: 'meteor',
   webDir: 'build',
   bundledWebRuntime: false
